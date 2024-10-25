930fcae7b6f3e78e953ba8674bd82ef38eb5d50e
mikelehu <mikel.antonana@gmail.com>
2024-10-25 20:48:25 +0200

    my first package

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
-using FirstSentence2
+using .FirstSentence2
 using Test
 
 @testset "FirstSentence2.jl" begin
     # Write your tests here.
+    
+    str = "Hello, world!"
+    @test firstsentence(str) == str
+
+    str = "Hello, class. We are covering testing."
+    @test firstsentence(str) == "Hello, class."
+    
 end
